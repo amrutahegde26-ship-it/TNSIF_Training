b478d47f6d856074aaab7e5b309f582b56415ca5
Hegde <Hegde@Home>
2026-07-28 10:13:38 +0530

    multilevel inheritance

---
+++ TNSIF_Training/src/org/tnsif/acce/c2tc/oops/MultilevelInheritanceDemo.java
@@ -0,0 +1,30 @@
+package org.tnsif.acce.c2tc.oops;
+
+class Device{
+	void deviceType() {
+		System.out.println("i am an electronic device");
+	}
+}
+class Phone extends Device{
+	void phoneType() {
+		System.out.println("my phone is android phone");
+	}
+}
+class Laptop extends Phone{
+	void laptopType() {
+		System.out.println("it is lenovo laptop");
+	}
+}
+
+public class MultilevelInheritanceDemo {
+
+	public static void main(String[] args) {
+		Laptop laptop=new Laptop();
+		laptop.deviceType();
+		laptop.phoneType();
+		laptop.laptopType();
+		
+
+	}
+
+}
