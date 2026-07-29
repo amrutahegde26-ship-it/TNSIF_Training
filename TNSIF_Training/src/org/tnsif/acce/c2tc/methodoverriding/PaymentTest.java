323bf5711b339f424560b2b0d9d2d4fe84f64943
Hegde <Hegde@Home>
2026-07-29 09:50:07 +0530

    method overriding

---
+++ TNSIF_Training/src/org/tnsif/acce/c2tc/methodoverriding/PaymentTest.java
@@ -0,0 +1,40 @@
+package org.tnsif.acce.c2tc.methodoverriding;
+class Payment{
+	void makePayment() {
+		System.out.println("processing payment uing generic method");
+	}
+	
+}
+class UpiPayment extends Payment{
+	void makePayment() {
+		System.out.println("payment received by upi");
+	}
+}
+class CardPayment extends Payment{
+	void makePaymen() {
+		System.out.println("payment is done using card");
+	}
+}
+class WalletPayment extends Payment{
+	void makePayment() {
+		System.out.println("payment done via wallet");
+	}
+}
+
+public class PaymentTest {
+
+	public static void main(String[] args) {
+		Payment payment; //reference parent class
+		payment =new UpiPayment();
+		payment.makePayment();
+		payment=new CardPayment();
+		payment.makePayment();
+		
+		
+		payment=new WalletPayment();
+		payment.makePayment();
+		
+
+	}
+
+}
