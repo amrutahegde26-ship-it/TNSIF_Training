0d0415eab9054a9f1f65dc4ce6655cf2e8f04c0a
Hegde <Hegde@Home>
2026-07-29 10:07:15 +0530

    super with variable

---
+++ TNSIF_Training/src/org/tnsif/acce/c2tc/methodoverriding/SuperWithVariable.java
@@ -0,0 +1,23 @@
+package org.tnsif.acce.c2tc.methodoverriding;
+class Notification{
+	String message="Default Notification";
+	
+}
+class PushNotification extends Notification{
+	String message="push Notification";
+	void showNotification() {
+		System.out.println("child message :" +message);
+		System.out.println("parent message:"+super.message);
+	}
+}
+
+public class SuperWithVariable {
+
+	public static void main(String[] args) {
+		PushNotification obj=new PushNotification();
+		obj.showNotification();
+		
+
+	}
+
+}
