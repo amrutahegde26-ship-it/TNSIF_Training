97b47e098d8c4a1ec58ad0493b36eabb50ce4b82
Hegde <Hegde@Home>
2026-08-14 11:45:57 +0530

    string index out of bound exception

---
+++ TNSIF_Training/src/org/tnsif/acce/c2tc/exceptionHandling/StringIndexOutOfBoundDemo.java
@@ -0,0 +1,17 @@
+package org.tnsif.acce.c2tc.exceptionHandling;
+
+public class StringIndexOutOfBoundDemo {
+
+	public static void main(String[] args) {
+		String str="hello";
+		try {
+		
+			char ch=str.charAt(5);
+			System.out.println(ch);
+		}catch(StringIndexOutOfBoundsException e) {
+			System.out.println(e);
+		
+		}
+	}
+
+}
